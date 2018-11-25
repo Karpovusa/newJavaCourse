4aacf7c5f488c251691eda5213d5794211fed692
Ilya <karpovusa@gmail.com>
2018-11-25 12:14:55 +0300

    Задание 1

--- sandbox/scr/main/java/ru/stqa/pft/MyFirstProgram.java
+++ sandbox/scr/main/java/ru/stqa/pft/MyFirstProgram.java
@@ -3,20 +3,15 @@ package ru.stqa.pft.sandbox;
 import java.net.SocketTimeoutException;
 
 class MyFirstProgram{
-    public static void main(String[] args){
-        hello("world");
-        hello("user");
-        hello("Alexei");
-        Square s = new Square(5);
-        System.out.println("площадь квадрата со стороной " + s.l + "равна " + s.area());
 
-        Rectangle r = new Rectangle(4,6);
-        System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b +" равна " + r.area());
-    }
-    public	static void hello(String somebody ) {
 
-        System.out.println("Hello " + somebody + " !");
-    }
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello World!");
+
 
 
 }
