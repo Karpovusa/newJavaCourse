d6f5944c289eff8d98d5b3db5df52e8853b9a5b2
Ilya <karpovusa@gmail.com>
2018-12-02 22:44:05 +0300

    правка браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -14,7 +14,7 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd = new FirefoxDriver;
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://192.168.0.2/addressbook/");
         groupHelper = new GroupHelper(wd);
