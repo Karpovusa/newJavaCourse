b481c0108e81d572ab382fafe364c3c53e0fd771
Ilya <karpovusa@gmail.com>
2019-01-26 14:58:16 +0300

    задание 17

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,69 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+
+public class HelperBase {
+    WebDriver wd;
+    ApplicationManager app;
+
+    public HelperBase(ApplicationManager app) {
+        this.app = app;
+        this.wd=app.getDriver();
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try{
+            wd.findElement(locator);
+            return true;
+        }
+        catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    public void acceptDialogueWindow() {
+        wd.switchTo().alert().accept();
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+    public void select(By locator, String text){
+        Select select= new Select(wd.findElement(locator));
+        if(elementExistsInDropdown(select,text)) select.selectByVisibleText(text);
+    }
+
+    public boolean elementExistsInDropdown(Select select, String text){
+        List<WebElement> DrpDwnList=select.getOptions();
+       for(WebElement element:DrpDwnList){
+            if (element.getText().contains(text))
+                return true;
+            }
+           return false;
+
+    }
+}
